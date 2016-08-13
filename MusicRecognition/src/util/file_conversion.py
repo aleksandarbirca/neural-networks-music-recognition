2dd976f0a70f03df6f3ca6dc8fba2d59684c1bf9
Aleksandar Bircakovic <a.bircakovic@levi9.com>
2016-08-13 21:16:58 +0200

    Extracted Mel Frequency Cepstral Coefficients from WAV file.

--- MusicRecognition/src/util/file_conversion.py
+++ MusicRecognition/src/util/file_conversion.py
@@ -1,6 +1,11 @@
 import os
 import timeit
+import scipy.io.wavfile
+import scipy
+import numpy as np
 from pydub import AudioSegment
+from scikits.talkbox.features import mfcc
+
 
 config = {}
 execfile("..\..\config.cfg", config)
@@ -29,9 +34,35 @@ def convert_dataset_to_wav():
                 os.remove(path)
 
     stop = timeit.default_timer()
-    print "Files converted Successfully."
+    print "Files converted successfully."
+    print "Conversion time = ", (stop - start)
+
+# Mel Frequency Cepstral Coefficients
+def convert_wav_to_mfcc():
+    start = timeit.default_timer()
+    print "Starting conversion to MFCC."
+    for subdir, dirs, files in os.walk(DATASET_DIR):
+        for file in files:
+            path = subdir+'/'+file
+            if path.endswith("wav"):
+                # TODO: Check if GENRE_LIST property contains folder name (genre)
+                extract_cepstrum(path) # Mel-frequency cepstrum (https://en.wikipedia.org/wiki/Mel-frequency_cepstrum)
+
+    stop = timeit.default_timer()
+    print "Files converted successfully."
     print "Conversion time = ", (stop - start)
 
 
+def extract_cepstrum(path):
+    sample_rate, X = scipy.io.wavfile.read(path)
+    # X[X == 0] = 1
+    ceps, mspec, spec = mfcc(X)
+    base_fn, ext = os.path.splitext(path)
+    data_fn = base_fn + ".ceps"
+    np.save(data_fn, ceps)
+    print "Written ", data_fn
+
+
 if __name__ == "__main__":
-    convert_dataset_to_wav()
+    # convert_dataset_to_wav()
+    convert_wav_to_mfcc()
