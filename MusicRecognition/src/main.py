Fixed issue with input normalization.

MusicRecognition/src/main.py
@@ -34,9 +34,9 @@ class DialogWindow(QtGui.QWidget):
         print 'Opened file ' + filename
 
         filename = str(filename)
-        song = AudioSegment.from_file(filename, "mp3")
+        song = AudioSegment.from_file(filename, "au")
         song = song[:30000]
-        wav_file = song.export(filename[:-3] + "wav", format='wav').name
+        wav_file = song.export(filename[:-2] + "wav", format='wav').name
         sample_rate, data = scipy.io.wavfile.read(wav_file)
         data[data == 0] = 1
         os.remove(wav_file)
@@ -44,11 +44,13 @@ class DialogWindow(QtGui.QWidget):
         num_ceps = len(ceps)
         X = []
         X.append(np.mean(ceps[0:num_ceps], axis=0))
-        X = scale(X, axis=0, with_mean=True, with_std=True, copy=True)
+        X = np.array(X)
+        X = scale(X, axis=1, with_mean=True, with_std=True, copy=True)
         model = model_from_json(open('..\data\weights\model.json','r').read())
         model.load_weights('..\data\weights\weights.h5')
 
-        Y = model.predict(np.array(X))
+        Y = model.predict(X)
+        self.textbox.clear()
         self.textbox.insertPlainText('Blues: ' + str(Y[0][0]))
         self.textbox.insertPlainText('\nClassical: ' + str(Y[0][1]))
         self.textbox.insertPlainText('\nCountry: ' + str(Y[0][2]))

MusicRecognition/src/nn/training.py
@@ -33,11 +33,9 @@ def train_network():
     X, Y = read_mfcc()
     print '\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
     print '\nTraining network started\n'
-    X = scale( X, axis=0, with_mean=True, with_std=True, copy=True )
-    Y = scale( Y, axis=0, with_mean=True, with_std=True, copy=True )
+    X = scale( X, axis=1, with_mean=True, with_std=True, copy=True )
     #X = (X - np.min(X)) / (np.max(X) - np.min(X))
-    #Y = (Y - np.min(Y)) / (np.max(Y) - np.min(Y))
-    model.fit(X, Y, nb_epoch=10000, batch_size=128,  validation_data=(X, Y))
+    model.fit(X, Y, nb_epoch=5000, batch_size=128,  validation_data=(X, Y))
     model.save_weights('..\..\data\weights.h5', overwrite=True)
     score = model.evaluate(X, Y)
     print 'Network trained successfully and network weights saved as file weights.h5.'
@@ -47,7 +45,7 @@ def train_network():
 
 def read_mfcc():
     X = []
-    y = []
+    Y = []
     for label, genre in enumerate(GENRE_LIST):
         for file in glob.glob(os.path.join(DATASET_DIR, genre, "*.ceps.npy")):
             print 'Extracting MFCC from ' + file
@@ -56,8 +54,8 @@ def read_mfcc():
             X.append(np.mean(ceps[0:num_ceps], axis=0))
             g = np.zeros(10)
             g[label.real] = 1
-            y.append(g)
-    return np.array(X), np.array(y)
+            Y.append(g)
+    return np.array(X), np.array(Y)
 
 if __name__ == "__main__":
     compile_model()