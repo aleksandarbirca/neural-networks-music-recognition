e6eadc4efc007e1f73db02d0a72c69a67b439727
Aleksandar Bircakovic <a.bircakovic@levi9.com>
2016-08-31 14:07:52 +0200

    Improved LSTM validation accuracy to 80% on 6 genre classification.

--- MusicRecognition/src/nn/training_lstm_mfcc.py
+++ MusicRecognition/src/nn/training_lstm_mfcc.py
@@ -1,10 +1,10 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, LSTM
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam
 import os
 import numpy as np
 import glob
-from sklearn.preprocessing import scale
+from keras.callbacks import ModelCheckpoint
 
 
 config = {}
@@ -15,21 +15,23 @@ TEST_DIR = config["TEST_DIR"]
 
 model = Sequential()
 
-num_of_mfcc = 100
+num_of_mfcc = 40
+num_of_ceps = 13 * 4
 
 
 def compile_lstm_model():
-    model.add(LSTM(512, return_sequences=True, activation='tanh', input_shape=(num_of_mfcc, 13)))
-    model.add(Dropout(0.1))
-    model.add(LSTM(512, activation='tanh', return_sequences=False))
-    model.add(Dropout(0.1))
+    model.add(LSTM(512, return_sequences=True, input_shape=(num_of_mfcc, num_of_ceps)))
+    model.add(Dropout(0.25))
+    model.add(LSTM(512, return_sequences=False))
+    model.add(Dropout(0.25))
     model.add(Dense(10))
     model.add(Activation('softmax'))
-    #optimizer = RMSprop(lr=0.9)
-    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    optimizer = RMSprop(lr=0.0001)
+    #optimizer = Adam(lr=0.0001)
+    model.compile(optimizer=optimizer, loss='mae', metrics=['accuracy'])
 
     json_model = model.to_json()
-    open('..\..\data\model.json', 'w').write(json_model)
+    open('..\..\data\model_lstm_mfcc.json', 'w').write(json_model)
     print ('\nNetwork created successfully and network model saved as file model.json.')
 
 
@@ -37,13 +39,18 @@ def train_network():
     x, y = read_mfcc(DATASET_DIR)
     x_test, y_test = read_mfcc(TEST_DIR)
 
-    x = np.reshape(x, (x.shape[0], x.shape[1], 13))
-    x_test = np.reshape(x_test, (x_test.shape[0], x.shape[1], 13))
+    normalize_cepstrum_coefficients(x)
+    normalize_cepstrum_coefficients(x_test)
+
+    x = np.reshape(x, (x.shape[0], x.shape[1], num_of_ceps))
+    x_test = np.reshape(x_test, (x_test.shape[0], x.shape[1], num_of_ceps))
 
     print ('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print ('\nTraining network started\n')
-    model.fit(x, y, nb_epoch=1000, batch_size=16, validation_data=(x_test, y_test))
-    model.save_weights('..\..\data\weights.h5', overwrite=True)
+    print ('\nNetwork training started\n')
+    checkpointer = ModelCheckpoint(filepath="..\..\data\weights_temp_lstm_mfcc.hdf5", verbose=1, save_best_only=True,
+                                   monitor='val_acc', mode='auto')
+    model.fit(x, y, nb_epoch=1000, batch_size=16, validation_data=(x_test, y_test), callbacks=[checkpointer])
+    model.save_weights('..\..\data\weights_lstm_mfcc.h5', overwrite=True)
     score = model.evaluate(x_test, y_test)
     print ('\nNetwork trained successfully and network weights saved as file weights.h5.')
     print('\nTest score:', score[0])
@@ -57,9 +64,17 @@ def read_mfcc(data_dir):
         for file in glob.glob(os.path.join(data_dir, genre, "*.ceps.npy")):
             print ('Extracting MFCC from ' + file)
             ceps = np.load(file)
-            ceps = ceps[0:num_of_mfcc]
-            normalize_cepstrum_coefficients(ceps)
-            x.append(np.array(ceps))
+            sampled_ceps = []
+            for i in range(0, len(ceps)/100):
+                temp = []
+                temp.extend(np.mean(ceps[i*100:i*100+100], axis=0))
+                temp.extend(np.min(ceps[i*100:i*100+100], axis=0))
+                temp.extend(np.max(ceps[i*100:i*100+100], axis=0))
+                temp.extend(np.var(ceps[i * 100:i * 100 + 100], axis=0))
+                sampled_ceps.append(temp)
+
+            sampled_ceps = sampled_ceps[0:num_of_mfcc]
+            x.append(np.array(sampled_ceps))
             g = np.zeros(10)
             g[label.real] = 1
             y.append(g)
@@ -67,8 +82,8 @@ def read_mfcc(data_dir):
 
 
 def normalize_cepstrum_coefficients(x):
-    x[:, 0] = x[:, 0] / 20
-    x[:, 1:13] = x[:, 1:13] / 3
+    x -= np.mean(x, axis=0)
+    x /= np.std(x, axis=0)
     return x
 
 if __name__ == "__main__":
